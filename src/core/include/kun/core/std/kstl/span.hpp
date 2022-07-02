00a8bcb6dba9f6b6f37e83127e385818c8c590d9
zhurong <211103376@qq.com>
2022-07-02 15:59:22 +0800

    impl span

--- src/core/include/kun/core/std/kstl/span.hpp
+++ src/core/include/kun/core/std/kstl/span.hpp
@@ -1,48 +1,160 @@
 #pragma once
 #include "kun/core/config.h"
+#include "algo/find.hpp"
 #include "kun/core/std/types.hpp"
 
+// Span def
 namespace kun
 {
 template<typename T> class Span
 {
 public:
-    KUN_INLINE Span() noexcept
-        : m_ptr{nullptr}
-        , m_size{0}
-    {}
-    KUN_INLINE Span(T* ptr, Size len) noexcept
-        : m_ptr{ptr}
-        , m_size{len}
-    {}
-    template<Size N>
-    KUN_INLINE Span(T (&arr)[N]) noexcept
-        : m_ptr(&arr[0])
-        , m_size{N}
-    {}
+    // ctor & dtor
+    Span();
+    Span(T* p, Size len);
+    template<Size N> Span(T (&arr)[N]);
+    ~Span();
 
-    // index access
-    KUN_INLINE T& operator[](Size i) const noexcept { return *m_ptr[i]; }
+    // copy & move & assign & move assign
+    Span(const Span& other);
+    Span(Span&& other);
+    Span& operator=(const Span& other);
+    Span& operator=(Span&& other);
 
-    // size and data
-    KUN_INLINE T* data() const noexcept { return m_ptr; }
-    KUN_INLINE Size size() const noexcept { return m_size; }
+    // compare
+    bool operator==(const Span& rhs) const;
+    bool operator!=(const Span& rhs) const;
 
-    // front & back
-    KUN_INLINE T& front() const noexcept { return *m_ptr; }
-    KUN_INLINE T& back() const noexcept { return *(m_ptr + m_size - 1); }
+    // getter
+    T*   data() const;
+    Size size() const;
 
-    // sub view
-    KUN_INLINE Span<T> first(Size count) { return Span<T>(m_ptr, count); }
-    KUN_INLINE Span<T> back(Size count) { return Span<T>(m_ptr + (m_size - count), count); }
-    KUN_INLINE Span<T> subSpan(Size offset, Size count) { return Span<T>(m_ptr + offset, count); }
+    // validate
+    bool isValidIndex(Size idx) const;
+    bool isValidPointer(const T* p) const;
+
+    // sub span
+    Span<T> left(Size pos) const;
+    Span<T> right(Size pos) const;
+    Span<T> subSpan(Size pos, Size n) const;
+
+    // find
+    template<typename TK> T* find(const TK& v) const;
+    template<typename TK> T* findLast(const TK& v) const;
+
+    // find if
+    template<typename TP> T* findIf(TP&& p) const;
+    template<typename TP> T* findLastIf(TP&& p) const;
+
+    // contain
+    bool contain(const T& v) const;
+
+    // contain if
+    template<typename TP> bool containIf(TP&& p) const;
+
+    // top & bottom
+    T& top() const;
+    T& bottom() const;
 
     // foreach support
-    KUN_INLINE T* begin() noexcept { return m_ptr; }
-    KUN_INLINE T* end() noexcept { return m_ptr + m_size; }
+    T* begin() const;
+    T* end() const;
 
 private:
-    T* m_ptr;
+    T*   m_data;
     Size m_size;
 };
+}// namespace kun
+
+// Spam impl
+namespace kun
+{
+// ctor & dtor
+template<typename T>
+KUN_INLINE Span<T>::Span()
+    : m_data(nullptr)
+    , m_size(0)
+{
+}
+template<typename T>
+KUN_INLINE Span<T>::Span(T* p, Size len)
+    : m_data(p)
+    , m_size(len)
+{
+}
+template<typename T>
+template<Size N>
+KUN_INLINE Span<T>::Span(T (&arr)[N])
+    : m_data(arr)
+    , m_size(N)
+{
+}
+template<typename T> KUN_INLINE Span<T>::~Span() = default;
+
+// copy & move & assign & move assign
+template<typename T> KUN_INLINE Span<T>::Span(const Span& other) = default;
+template<typename T> KUN_INLINE Span<T>::Span(Span&& other) = default;
+template<typename T> KUN_INLINE Span<T>& Span<T>::operator=(const Span& other) = default;
+template<typename T> KUN_INLINE Span<T>& Span<T>::operator=(Span&& other) = default;
+
+// compare
+template<typename T> KUN_INLINE bool Span<T>::operator==(const Span& rhs) const { return m_data == rhs.m_data && m_size == rhs.size(); }
+template<typename T> KUN_INLINE bool Span<T>::operator!=(const Span& rhs) const { return !(*this == rhs); }
+
+// getter
+template<typename T> KUN_INLINE T*   Span<T>::data() const { return m_data; }
+template<typename T> KUN_INLINE Size Span<T>::size() const { return m_size; }
+
+// validate
+template<typename T> KUN_INLINE bool Span<T>::isValidIndex(Size idx) const { return idx >= 0 && idx < m_size; }
+template<typename T> KUN_INLINE bool Span<T>::isValidPointer(const T* p) const { return p >= begin() && p < end(); }
+
+// sub span
+template<typename T> KUN_INLINE Span<T> Span<T>::left(Size pos) const
+{
+    KUN_Assert(pos < m_size);
+    return Span(m_data, pos);
+}
+template<typename T> KUN_INLINE Span<T> Span<T>::right(Size pos) const
+{
+    KUN_Assert(pos < m_size);
+    return pos < m_size - 1 ? Span(m_data + pos, m_size - pos) : Span();
+}
+template<typename T> KUN_INLINE Span<T> Span<T>::subSpan(Size pos, Size n) const
+{
+    KUN_Assert(pos < m_size && pos + n < m_size);
+    return Span(m_size + pos, n);
+}
+
+// find
+template<typename T> template<typename TK> KUN_INLINE T* Span<T>::find(const TK& v) const
+{
+    return findIf([&v](const T& a) { return a == v; });
+}
+template<typename T> template<typename TK> KUN_INLINE T* Span<T>::findLast(const TK& v) const
+{
+    return findLastIf([&v](const T& a) { return a == v; });
+}
+
+// find if
+template<typename T> template<typename TP> KUN_INLINE T* Span<T>::findIf(TP&& p) const { return algo::find(begin(), end(), std::forward<TP>(p)); }
+template<typename T> template<typename TP> KUN_INLINE T* Span<T>::findLastIf(TP&& p) const
+{
+    return algo::findLast(begin(), end(), std::forward<TP>(p));
+}
+
+// contain
+template<typename T> KUN_INLINE bool Span<T>::contain(const T& v) const { return find(v); }
+
+// contain if
+template<typename T> template<typename TP> KUN_INLINE bool Span<T>::containIf(TP&& p) const { return findIf(std::forward<TP>(p)); }
+
+// top & bottom
+template<typename T> KUN_INLINE T& Span<T>::top() const { return *(m_data + m_size - 1); }
+template<typename T> KUN_INLINE T& Span<T>::bottom() const { return *m_data; }
+
+// foreach support
+template<typename T> KUN_INLINE T* Span<T>::begin() const { return m_data; }
+template<typename T> KUN_INLINE T* Span<T>::end() const { return m_data + m_size; }
+
 }// namespace kun
