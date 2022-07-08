2cff11d5510d2b53595c634c3432f705dd7db8f6
zhurong <211103376@qq.com>
2022-07-08 20:19:09 +0800

    USet basic def

--- src/core/include/kun/core/std/kstl/container/fwd.hpp
+++ src/core/include/kun/core/std/kstl/container/fwd.hpp
@@ -4,12 +4,17 @@
 namespace kun
 {
 class PmrAllocator;
+using DefaultAllocator = PmrAllocator;
 }
 
 // containers
 namespace kun
 {
-template<typename Alloc = PmrAllocator> class BitArray;
-template<typename T, typename Alloc = PmrAllocator> class Array;
-template<typename T, typename Alloc = PmrAllocator> class SparseArray;
+template<typename Alloc = DefaultAllocator> class BitArray;
+template<typename T, typename Alloc = DefaultAllocator> class Array;
+template<typename T, typename Alloc = DefaultAllocator> class SparseArray;
+
+template<typename T, bool MultiKey = false> struct USetConfigDefault;
+template<typename T, typename Alloc = DefaultAllocator, typename Config = USetConfigDefault<T>> class USet;
+
 }// namespace kun
--- src/core/include/kun/core/std/kstl/container/uset.hpp
+++ src/core/include/kun/core/std/kstl/container/uset.hpp
@@ -4,9 +4,88 @@
 #include "sparse_array.hpp"
 #include "fwd.hpp"
 
+// USet config
 namespace kun
 {
-template<typename T, typename Alloc, typename KeyFunc> class USet
+template<typename T, bool MultiKey> struct USetConfigDefault
 {
+    using KeyType = T;
+    using HashType = Size;
+
+    static constexpr bool multi_key = MultiKey;
+
+    KUN_INLINE static const KeyType& key(const T& v) { return v; }
+    KUN_INLINE static bool           equal(const KeyType& a, const KeyType& b) { return a == b; }
+    KUN_INLINE static HashType       hash(const KeyType& k) { return Hash<KeyType>()(k); }
+};
+}// namespace kun
+
+// USet element
+namespace kun
+{
+template<typename T, typename TS, typename HashType> struct USetData
+{
+    T          data;
+    HashType   hash;
+    mutable TS next;
+};
+}// namespace kun
+
+// USet def
+namespace kun
+{
+template<typename T, typename Alloc, typename Config> class USet
+{
+public:
+    using SizeType = typename Alloc::SizeType;
+    using HashType = typename Config::HashType;
+    using KeyType = typename Config::KeyType;
+    using DataType = USetData<T, SizeType, HashType>;
+
+public:
+    // ctor & dtor
+
+    // copy & move
+
+    // assign & move assign
+
+    // compare
+
+    // getter
+
+    // validate
+
+    // memory op
+
+    // add
+
+    // add as
+
+    // emplace
+
+    // append
+
+    // remove
+
+    // remove as
+
+    // use as sparse array? remove all & remove by
+
+    // modify
+
+    // find
+
+    // find as
+
+    // contain
+
+    // sort
+
+    // static? set ops
+
+private:
+    mutable SizeType*            m_bucket;
+    mutable SizeType             m_bucket_size;
+    SparseArray<DataType, Alloc> m_data;
 };
 }// namespace kun
